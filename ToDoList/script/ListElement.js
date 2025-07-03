5c4771a58b8f4748385a662aefe8c72be27d6f87
Roger Miguel Staudt <rogermiguelstaudt@gmail.com>
2025-07-02 21:34:59 -0300

    add option has been placed

--- ToDoList/script/ListElement.js
+++
@@ -1,6 +0,0 @@
-class ItemList {
-    constructor(text, state) {
-        this.text = text;
-        this.state = state;
-    }
-}
--- ToDoList/script/script.js
+++ ToDoList/script/script.js
@@ -1,3 +1,56 @@
-function createItem() {
+class ItemList {
+    constructor(text, state) {
+        this.text = text
+        this.state = state
+    }
+}
+
+var list = [
+    {
+        text: "Exemplo 1",
+        state: "to-do"
+    },
+    {
+        text: "Exemplo 2",
+        state: "to-do"
+    }
+]
+
+
+function addItem() {
+
+    const newItem = new ItemList
+
+    newItem.text = ""
+    newItem.state = "to-do"
+
+    list.push(newItem)
+
+    createElement();
+}
+
+function deleteItem() {
+
+}
     
+function createElement() {
+    var listRegion = document.getElementById('todo-list');
+
+    let li = document.createElement('li')
+
+    let checkbox = document.createElement('input')
+    checkbox.type = "checkbox"
+
+    let textField = document.createElement('input')
+    textField.type = "text"
+
+    let trashIcon = document.createElement('i')
+    trashIcon.classList.add('bi')
+    trashIcon.classList.add('bi-trash-fill')
+
+    listRegion.appendChild(li)
+
+    li.appendChild(checkbox)
+    li.appendChild(textField)
+    li.appendChild(trashIcon)
 }
